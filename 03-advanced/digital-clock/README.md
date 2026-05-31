34ad7174ca32acedd853e45ae76adc33bfc568b9
Namiez Asfar <namiez.asfar79@gmail.com>
2026-05-31 15:43:15 +0530

    Added digital-clock to advanced C projects

---
+++ 03-advanced/digital-clock/README.md
@@ -0,0 +1,30 @@
+# Digital Clock ⏲️
+
+A digital clock simulation built in C.
+
+## 📋 Features
+- Has seperate files for Windows and Linux
+- 24 hour clock
+
+## 🛠️ How to Run
+```bash
+gcc digital-clock-default.c -o digital-clock-default
+./digital-clock
+```
+
+## 📖 How to Use
+1. Run the program
+2. Clock starts running
+
+## 💡 Concepts Used
+- Loops and conditions
+- Functions
+- Multiple libraries
+
+## 📸 Sample Output
+DIGITAL CLOCK
+12:20:25
+
+## 👤 Author
+Namiez Asfar
+[GitHub](https://github.com/namiezasfar7)
---
+++ 03-advanced/digital-clock/digital-clock-windows.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <windows.h>
+
+int main(){
+
+    //DECLARE VARIABLES
+    time_t rawtime = 0; // Epoch Time (Jan 1 1970)
+    struct tm *pTime = NULL;
+    bool isRunning = true;
+
+    //WELCOME MESSAGE
+    printf("DIGITAL CLOCK\n");
+
+    //LOOP UNTIL CONDITION IS TRUE
+    while(isRunning){
+
+        //GET TIME
+        time(&rawtime);
+
+        //POINT TO STRUCT tm
+        pTime = localtime(&rawtime);
+
+        //PRINT TIME
+        printf("\r%02d:%02d:%02d", pTime->tm_hour, pTime->tm_min, pTime->tm_sec);
+
+        //UPDATES EVERY 1 SECOND
+        Sleep(1000);
+    }
+
+    return 0;
+}
